9c7a93edfb0a9b0f93ed571c1526790b91a89947
Justin Chadwell <jedevc@gmail.com>
2015-05-29 18:03:37 +0545

    Improved program halting.

--- euphoria/room.py
+++ euphoria/room.py
@@ -59,11 +59,11 @@ class Room:
             
         self.connection.close()
         
-    def __sigterm_handler(self, signal, frame):
+    def __exit_program(self, signal, frame):
         """
-        sigterm_handler(signal, frame) -> None
+        __exit_program(signal, frame) -> None
         
-        Use to quit properly when there is a SIGTERM. NOT to be called directly.
+        Use to quit properly when there is the program is terminated.
         """
 
         self.quit()
@@ -80,29 +80,25 @@ class Room:
         attempts = 0
         
         #Handle process kills.
-        signal.signal(signal.SIGTERM, self.__sigterm_handler)
+        signal.signal(signal.SIGTERM, self.__exit_program)
+        signal.signal(signal.SIGINT, self.__exit_program)
         
         while 1:
-            try:
-                #Check for multiple failures in a row
-                if attempts >= 2:
-                    self.quit()
-                    return
-                
-                if self.connection.receive_data():
-                    attempts = 0
-                else:
-                    #No connection initialized
-                    if first:
-                        first = False
-                    else:
-                        time.sleep(5)
-                        attempts += 1
-                        
-                    self.join()
-                    self.identify()
-                    self.ready()
-            except KeyboardInterrupt:
-                #User halt
+            #Check for multiple failures in a row
+            if attempts >= 2:
                 self.quit()
                 return
+            
+            if self.connection.receive_data():
+                attempts = 0
+            else:
+                #No connection initialized
+                if first:
+                    first = False
+                else:
+                    time.sleep(5)
+                    attempts += 1
+                    
+                self.join()
+                self.identify()
+                self.ready()
