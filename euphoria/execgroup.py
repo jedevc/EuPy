1c2fae99b7fcac683236c96bbc57ef3ae2006fe3
Justin Chadwell <jedevc@gmail.com>
2015-07-09 16:27:53 +0100

    Made autokill for groups optional.

--- euphoria/execgroup.py
+++ euphoria/execgroup.py
@@ -9,12 +9,14 @@ class ExecGroup(executable.Executable):
     at the same time.
     """
 
-    def __init__(self):
+    def __init__(self, autostop=True):
         self.execs = []
         self.exec_threads = []
 
         self.lock = threading.Lock()
         self.running = False
+
+        self.autostop = autostop
 
     def add(self, toadd):
         """
@@ -44,13 +46,19 @@ class ExecGroup(executable.Executable):
         for e in self.exec_threads:
             e.start()
 
-        while self.running and len(self.execs) != 0:
+        while self.running:
+            if self.autostop and len(self.execs) == 0:
+                break
+
+            #Iterate backwards and remove dead threads
             for i in range(len(self.execs) - 1, -1, -1):
                 if not self.execs[i].running:
                     self.exec_threads[i].join()
                     self.exec_threads.remove(self.exec_threads[i])
                     self.execs.remove(self.execs[i])
             time.sleep(2)
+
+        self.running = False
 
     def quit(self):
         """
