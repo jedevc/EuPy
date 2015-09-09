c23bda05ada5877ae876d899ca69284cd42b57d1
Justin Chadwell <jedevc@gmail.com>
2015-09-09 07:22:40 +0545

    Fixed room authentication.

--- euphoria/room.py
+++ euphoria/room.py
@@ -32,7 +32,7 @@ class Room(executable.Executable):
             self.connection.connect(self.roomname)
 
             if self.password is not None:
-                self.connection.send_packet("auth", cn.build_json(passcode=self.password))
+                self.connection.send_packet("auth", cn.build_json(type="passcode", passcode=self.password))
 
     def identify(self):
         """
